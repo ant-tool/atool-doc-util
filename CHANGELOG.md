8c7c469e4f3bc87d4923948ed5c6633eb4fb7282
绯一 <jaredlee.ljj@alipay.com>
2016-04-21 13:59:02 +0800

    [feat] add class Resource to calculate path

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.0
+
+`2016-04-21`
+
+- add class Resource to calculate path
--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 
 module.exports = {
   marked: require('./lib/marked'),
+  Resource: require('./lib/resource'),
 };
